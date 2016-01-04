fcc9295c1aa597f837bfdb63e70981d668a46a91
ewnlm <luckjimmy829@gmail.com>
2016-01-04 23:33:16 +0800

    new add _str_cmp and _str_cpy

--- _string.h
+++ _string.h
@@ -6,10 +6,31 @@ char *_str_cat(char *des, char *src) {
   return ret;
 }
 
-char *_strchr(char *src, int character) {
-  while(*(src++)!=character) {
-    d++;
-  }
+char *_str_chr(char *src, int character) {
+  while(*(src++)!=character);
   src--;
   return *(src)?(src):(NULL);
+}
+
+int _str_cmp(char *str1, char *str2) {
+  while(*str1 && (*(str1)==*(str2))) {
+    str1--;
+    str2--;
+  }
+  return *(str1)-*(str2);
+}
+
+char  *_str_cpy(char *des, char *src) {
+  char	*result=des;
+  while(*(des)) {
+    des++;
+  }
+
+  while(*(src) && (*(des++)=*(src++)));
+
+  return result;
+}
+
+size_t _str_cspn(char *str1, char *str2) {
+
 }
--- main.c
+++ main.c
@@ -6,15 +6,24 @@ int main() {
   char	str2[] = "World";
   char	str3[] = "";
   char	str4[] = "Yes";
+  char	str5[] = "World";
+  char	str6[] = "wORLD";
+  char	str7[50]="String ";
   char	*ptr;
 
   printf("===strcat===\n");
   _str_cat(str1, str2);
   printf("-%s-\n", str1);
   printf("===strchr===\n");
-  ptr = _strchr(str2, 'W');
-  printf("%p %p\n", ptr, str2);
+  ptr = _str_chr(str2, 'W');
   printf("%zu\n", ptr-str2+1);
+  ptr = _str_chr(str2, 'r');
+  printf("%zu\n", ptr-str2+1);
+  printf("===strcmp===\n");
+  printf("Match: %d, %s %s\n", _str_cmp(str2, str5), str2, str5);
+  printf("Match: %d, %s %s\n", _str_cmp(str2, str6), str2, str6);
+  printf("===strcpy===\n");
+  printf("Copy: %s, (%s + %s)\n", str7, _str_cpy(str7, "Duplication"), "duplication");
 
   return 0;
 }
